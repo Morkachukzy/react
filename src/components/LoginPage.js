b8ba96dba97d0fa2673b28130347c7131514c9fd
Chukwuka Morka <61619525+Morkachukzy@users.noreply.github.com>
2022-09-21 16:14:37 +0100

    fix: router issues

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,53 +1,61 @@
 // CSS
-import '../css/LoginPage.css';
-
-
+import "../css/LoginPage.css";
+import { Link } from "react-router-dom";
 const SignIn = () => {
-    return (
-        <div className="signin-main">
-            <div>
-                <div className='title-h1'>
-                    <h1>SignIn Page</h1>
-                </div>
-                
-                <div className='title-h3'>
-                    <h3>Welcome back !!!</h3>
-                </div>
-                
-                <div className='Form-group'> 
-                    <label className='email'> 
-                        <input type='text' name='username' id='username' placeholder='Enter an email account: required'></input>
-                    </label>
-                </div>
-
-                <div className='Form-group'> 
-                    <label className='password'>
-                        <input type='text' name='username' id='username' placeholder='Enter your password: reuired'></input>
-                    </label>
-                </div>
-
-                <br/>
-
-                <div className="action"><button type="submit">Sign In</button></div>
-
-                        <br/>
-                        <br/>
-                        
-                <div className='button'>
-                    <span> 
-                        <text>don't have an account</text>
-                    </span>
-                    <span> 
-                        <button>Sign Up</button> 
-                    </span>
-
-                    <br/>
-                    <br/>
-                    
-                </div>
-            </div>
+  return (
+    <div className="signin-main">
+      <div>
+        <div className="title-h1">
+          <h1>SignIn Page</h1>
         </div>
-    )
+
+        <div className="title-h3">
+          <h3>Welcome back !!!</h3>
+        </div>
+
+        <div className="Form-group">
+          <label className="email">
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Enter an email account: required"
+            ></input>
+          </label>
+        </div>
+
+        <div className="Form-group">
+          <label className="password">
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Enter your password: reuired"
+            ></input>
+          </label>
+        </div>
+
+        <br />
+
+        <div className="action">
+          <button type="submit">Sign In</button>
+        </div>
+
+        <br />
+        <br />
+
+        <div className="button">
+          <span>Don't have an account</span>
+          <span>
+            <Link to="/">Sign Up</Link>
+          </span>
+
+          <br />
+          <br />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default SignIn 
+export default SignIn;
