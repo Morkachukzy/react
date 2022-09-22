f092bf339bd4cd8869550827c2372c11ca3c8ed8
Chukwuka Morka <61619525+Morkachukzy@users.noreply.github.com>
2022-09-22 13:07:44 +0100

    fix: form event handling

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -3,21 +3,29 @@ import "../css/SignupPage.css";
 import SignIn from "./LoginPage";
 import React from "react";
 import { Link } from "react-router-dom";
-import {useState} from 'react';
+import { useState } from "react";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [password2, setPassword2] = useState('');
+  const [userDetails, setUserDetails] = useState({
+    email: "",
+    name: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-  const getValueInput = event => {
-    console.log(username, email, password, password2);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUserDetails({ ...userDetails, [name]: value });
   };
-  
-  
+
+  const handleSignup = (event) => {
+    event.preventDefault();
+
+    console.log(userDetails);
+  };
+
   return (
-    <div className="signup-main">
+    <form className="signup-main" onSubmit={handleSignup}>
       <div>
         <div className="title-h1">
           <h1>Sign up Page</h1>
@@ -28,63 +36,57 @@ const Login = () => {
         </div>
 
         <div className="Form-group">
-          <label className="email">
-            <input
-              type="text"
-              name="email"
-              id="email"
-              placeholder="Enter an email account: required"
-              onChange={event => setUsername(event.target.value)}
-              required
-            ></input>
+          <label htmlFor="email" className="email">
+            Email Address
           </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={handleChange}
+            required
+          />
         </div>
 
         <div className="Form-group">
-          <label className="about_me">
-            <input
-              type="text"
-              name="about_me"
-              id="about_me"
-              placeholder='Enter an "about me": required'
-              onChange={event => setEmail(event.target.value)}
-              required
-            ></input>
-          </label>
+          <label htmlFor="name">Full Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={handleChange}
+            required
+          />
         </div>
 
         <div className="Form-group">
-          <label className="password">
-            <input
-              type="text"
-              name="password"
-              id="password"
-              placeholder="Enter a password: required"
-              onChange={event => setPassword(event.target.value)}
-              required
-            ></input>
-          </label>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={handleChange}
+            required
+          />
         </div>
 
         <div className="Form-group">
-          <label className="password2">
-            <input
-              type="password"
-              name="password2"
-              id="password2"
-              placeholder="Repeat password: required"
-              onChange={event => setPassword2(event.target.value)}
-              required
-            ></input>
-          </label>
+          <label className="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            onChange={handleChange}
+            required
+          />
         </div>
 
         <br />
 
         <div className="action">
-          <button type="submit" onClick={getValueInput}>Sign Up</button>
+          <button type="submit">Sign Up</button>
         </div>
-        
+
         <br />
         <br />
 
@@ -94,14 +96,13 @@ const Login = () => {
             <Link to="/login">Log in</Link>
           </span>
           <br />
-            <div id='output'>
-                <h4>REQUEST RESULTS</h4>
-                    <div id="api-response">
-                    </div>
-            </div>
+          <div id="output">
+            <h4>REQUEST RESULTS</h4>
+            <div id="api-response"></div>
+          </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
